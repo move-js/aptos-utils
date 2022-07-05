617caeae1bf681f1c50a8ba296dd1d2a42369b9b
Son Pin <hec.stw@gmail.com>
2022-07-06 02:43:16 +0700

    deploy MoonCoin and initialize

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ const figlet = require('figlet');
 const path = require('path');
 const program = require('commander');
 
+import { deployMoonCoin, fundAccount, initializeCoin } from './txs';
+import { private_key, rest_url, faucet_url } from '../aptos.json';
+import { AptosAccount, AptosClient } from 'aptos';
+
+const REST_URL = 'https://fullnode.devnet.aptoslabs.com';
+const FAUCET_URL = 'https://faucet.devnet.aptoslabs.com';
+
 clear();
 
 console.log(
@@ -38,8 +45,22 @@ program.command('create-coin')
   .option('-N, --name <name>', 'Name of coin')
   .option('-S, --symbol <symbol> ', 'Symbol of coin')
   .option('-D, --decimals <decimals> ', 'Decimals of coin')
-  .action((str: Object) => {
-    console.log(str);
+  .action(async (str: { name?: string, symbol?: string, decimals?: string }) => {
+    const client = new AptosClient(rest_url || REST_URL);
+    const privateKey = Uint8Array.from(
+      (private_key
+        .match(/.{1,2}/g) ?? [''])
+        .map((byte) => parseInt(byte, 16))
+    );
+  
+    const account = new AptosAccount(Uint8Array.from(privateKey));
+    console.log(account.address().toString());
+
+    await fundAccount(account.address().toString(), rest_url || REST_URL, faucet_url || FAUCET_URL);
+
+    await deployMoonCoin(client, account);
+
+    await initializeCoin(client, account, str.name || 'MoonCoin', str.symbol || 'MOON', str.decimals || '6');
   });
 
 program.command('mint-coin')
---
+++ src/txs.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-console */
+import { AptosClient, AptosAccount, FaucetClient, Types } from 'aptos';
+
+export const fundAccount = async (address: string, nodeUrl: string, faucetUrl: string) => {
+  const faucetClient = new FaucetClient(nodeUrl, faucetUrl);
+  await faucetClient.fundAccount(address, 5000);
+};
+
+export const deployMoonCoin = async (client: AptosClient, account: AptosAccount) => {
+  const payload: Types.TransactionPayload = {
+    type: 'module_bundle_payload',
+    modules: [{ bytecode:
+      '0x' +
+      'a11ceb0b0500000005010002020204070615081b200a3b05000000000000084d6f6f6e436f696e0b64756d6d795f6669656c64' +
+      account.address().toString().slice(-64) +
+      '000201010100'
+    }],
+  };
+  const txnRequest = await client.generateTransaction(account.address(), payload);
+  const signedTxn = await client.signTransaction(account, txnRequest);
+  const transactionRes = await client.submitTransaction(signedTxn);
+  await client.waitForTransaction(transactionRes.hash);
+};
+
+export const initializeCoin = async (client: AptosClient, account: AptosAccount, name: string, symbol: string, decimals: string): Promise<void> => {
+  const resources = await client.getAccountResources(account.address());
+  let payload: Types.TransactionPayload = {
+    type: "script_function_payload",
+    function: `0x1::ManagedCoin::initialize`,
+    type_arguments: [`0x${account.address().toString().slice(-64)}::MoonCoin::MoonCoin`],
+    arguments: [
+      Buffer.from(name, "utf-8").toString("hex"),
+      Buffer.from(symbol, "utf-8").toString("hex"),
+      decimals,
+      false,
+    ],
+  };
+  console.log(payload);
+  
+  const txnRequest = await client.generateTransaction(account.address(), payload);
+  const signedTxn = await client.signTransaction(account, txnRequest);
+  const transactionRes = await client.submitTransaction(signedTxn);
+  return await client.waitForTransaction(transactionRes.hash);
+};
